d5cc05b9890f22f346e8e570930eff2a3988d1a8
Qiang Wang <qiangwang@QiangdeMacBook-Pro.local>
2021-01-12 21:41:13 +0800

    update models.

---
+++ model.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import numpy as np
+import pickle
+from sklearn.linear_model import Lasso
+
+CORE_BASE = 1800
+MEM_BASE = 5000
+
+def LR(X, y, alpha = 0.05):    
+    
+    #lin = Lasso(alpha=0.0001,precompute=True,max_iter=1000, positive=True, random_state=9999, selection='random', fit_intercept=False)
+    lin = Lasso(alpha=0.01, positive=True, fit_intercept=False)
+    lin.fit(X,y)
+    print(lin.coef_, lin.intercept_)
+    w = lin.coef_
+    #w = np.array([lin.intercept_, lin.coef_[0], lin.coef_[1]])
+    print(w)
+
+    #inv_item = np.linalg.inv((np.dot(X.T, X)) + alpha * np.identity(X.shape[1]))
+    #w = np.dot((inv_item @ X.T), y)
+
+    return w
+
+df = pd.read_csv("csvs/gtx1080ti-dvfs-real-Performance-Power.csv", header = 0)
+print(df.columns)
+
+#extras = ['backpropBackward', 'binomialOptions', 'cfd', 'eigenvalues', 'gaussian', 'srad', 'dxtc', 'pathfinder', 'scanScanExclusiveShared', 'stereoDisparity'] 
+#df = df[~df.appName.isin(extras)]
+
+kernels = df['appName'].drop_duplicates()
+kernels.sort_values(inplace=True)
+
+para_dict = {}
+vp = 0.5
+time_err = 0
+power_err = 0
+p_basic = []
+gamma = []
+cg = []
+for kernel in kernels:
+
+    tmp_df = df[df.appName == kernel]
+
+    coreFs = tmp_df["coreF"].values * 1.0 / CORE_BASE
+    coreVs = (coreFs - vp) ** 2 * 2 + vp
+    memFs = tmp_df["memF"].values * 1.0 / MEM_BASE
+
+    # fitting performance
+    x1 = 1 / coreFs
+    x2 = 1 / memFs
+    X = np.stack((np.ones(x1.shape[0]), x1, x2)).T
+    y = tmp_df["time/ms"].values
+    print(X, y)
+    tw = LR(X, y)
+    t0 = tw[0]
+    D = tw[1] + tw[2]
+    delta = tw[1] / D
+    print("t0: %f, D: %f, delta: %f." % (t0, D, delta))
+    rec_y = D * (delta * x1 + (1 - delta) * x2) + t0
+    #rec_y = tw.predict(X)
+    time_err += np.mean((y - rec_y) ** 2)
+
+    # fitting power 
+    VFs = coreVs ** 2 * coreFs
+    X = np.stack((np.ones(memFs.shape[0]), memFs, VFs)).T
+    y = tmp_df["power/W"].values
+    print(X, y)
+    pw = LR(X, y)
+    p0 = pw[0]
+    gamma = pw[1]
+    cg = pw[2]
+    rec_y = p0 + gamma*memFs + cg*VFs
+    #rec_y = pw.predict(X)
+    power_err += np.mean((y - rec_y) ** 2)
+
+    para_dict[kernel] = {"p0":p0, "gamma":gamma, "cg":cg, "t0":t0, "D":D, "delta":delta}
+
+print("RMSE of performance:", time_err / len(kernels))
+print("RMSE of power:", power_err / len(kernels))
+print(para_dict)
+
+p0s = [v["p0"] for v in para_dict.values()]
+gammas = [v["gamma"] for v in para_dict.values()]
+cgs = [v["cg"] for v in para_dict.values()]
+t0s = [v["t0"] for v in para_dict.values()]
+Ds = [v["D"] for v in para_dict.values()]
+deltas = [v["delta"] for v in para_dict.values()]
+
+with open("apps.pkl", "wb") as fp:  
+    pickle.dump(para_dict, fp, protocol = pickle.HIGHEST_PROTOCOL)   
